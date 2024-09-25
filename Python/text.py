e6e70d630d8dd0b0f5834d08a8369a64c264d0ff
Liam J Gahan <liamgahan09@gmail.com>
2024-09-25 11:02:01 +0100

    Python classwork upload

---
+++ Python/text.py
@@ -0,0 +1,17 @@
+a = float(input())
+b = float(input())
+c = str(input())
+
+result = 0
+
+if (c == "Add"):
+    result = a + b
+if (c == "Subract"):
+    result = a - b
+if (c == "Times"):
+    result = a * b
+if (c == "Divide"):
+    result = a / b
+
+
+print(result)
