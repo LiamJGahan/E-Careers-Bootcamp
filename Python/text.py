93d6fd11b79b89e69e356b3c4b18add0c720b3d2
Liam J Gahan <liamgahan09@gmail.com>
2024-09-27 14:39:49 +0100

    Update text.py

--- Python/text.py
+++ Python/text.py
@@ -1,3 +1,13 @@
-import GetFactors
+import random
 
-print(GetFactors.get_factors_of_number(20))
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+list_2 = []
+
+list_1_count = len(list_1)
+
+for i in range(list_1_count):
+    random_num = random.choice(list_1)
+    list_2.append(random_num)
+    list_1.remove(random_num)
+
+print(list_2)
