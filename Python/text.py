cf4c0f88688e8bba38de9e0628007d622c027a4c
Liam J Gahan <liamgahan09@gmail.com>
2024-09-27 10:52:18 +0100

    Update text.py

--- Python/text.py
+++ Python/text.py
@@ -1,10 +1,8 @@
 list_1 = [5, 10, 15, 20, 25, 50,20]
 list_2 = []
 
-first_20 = True
-
 for num in list_1:
-    if  num == 20 and first_20 == True:
+    if  num == 20:
         continue
     else:
         list_2.append(num)
