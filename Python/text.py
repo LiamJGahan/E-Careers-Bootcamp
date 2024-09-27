f08042387820286289f5e59405641eb04491a07d
Liam J Gahan <liamgahan09@gmail.com>
2024-09-27 09:42:42 +0100

    Update text.py

--- Python/text.py
+++ Python/text.py
@@ -1,6 +1,9 @@
 number_list = [2, 4, 6, 3]
+squared_num_list = []
 
 for number in number_list:
     squared_num = number * number
-    print(squared_num)
+    squared_num_list.append(squared_num)
+
+print(squared_num_list)
 
