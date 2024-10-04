c47bacbfba895e6ae061e56e585ff785bdc5a83a
Liam J Gahan <liamgahan09@gmail.com>
2024-10-04 10:11:32 +0100

    Update text.py

--- Python/text.py
+++ Python/text.py
@@ -1,3 +1,11 @@
-fp = open("empty_text.txt", "a")
-fp.write("hi")
-fp.close()
+with open("empty_text.txt", "r") as fp:
+
+    lines = []
+    
+    for i, line in enumerate(fp):
+
+        if i < 2:
+            lines.append(line.strip())
+        else:
+            break
+print(lines)
