f5e643990fa4ead2df6114e62cca8c624fb2f957
Liam J Gahan <liamgahan09@gmail.com>
2024-10-04 10:39:22 +0100

    Update text.py

--- Python/text.py
+++ Python/text.py
@@ -3,9 +3,7 @@ with open("empty_text.txt", "r") as fp:
     lines = []
     
     for i, line in enumerate(fp):
+        
+        lines.append(line.strip())
 
-        if i < 2:
-            lines.append(line.strip())
-        else:
-            break
 print(lines)
