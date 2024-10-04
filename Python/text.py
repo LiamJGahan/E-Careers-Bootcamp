2e0d92f4a5b91a7026064721233e40d68d64e872
Liam J Gahan <liamgahan09@gmail.com>
2024-10-04 13:21:34 +0100

    Update text.py

--- Python/text.py
+++ Python/text.py
@@ -1,6 +1,7 @@
-with open("empty_text.txt", "r") as fp:
+list = ["red", "green", "blue"]
 
-    lines = fp.readlines()
-    lines.reverse()
-
-print(lines)
+try:
+    user_num = int(input("Enter username:"))
+    print(list[user_num])
+except IndexError:
+    print("Number given is out of index")
