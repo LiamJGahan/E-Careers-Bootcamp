1de2da4dfacb618730899a6632acc0e08491e1a5
Liam J Gahan <liamgahan09@gmail.com>
2024-10-04 09:54:08 +0100

    Testing pathways

--- Python/text.py
+++ Python/text.py
@@ -1,13 +1,3 @@
-import random
-
-list_1 = [1, 2, 3, 4, 5, 6, 7, 8]
-list_2 = []
-
-list_1_count = len(list_1)
-
-for i in range(list_1_count):
-    random_num = random.choice(list_1)
-    list_2.append(random_num)
-    list_1.remove(random_num)
-
-print(list_2)
+fp = open("empty_text.txt", "a")
+fp.write("hi")
+fp.close()
