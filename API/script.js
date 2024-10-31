4c0a31829f135e13953ceefe34a26e59ac5688ee
LiamJGahan <liamgahan09@gmail.com>
2024-10-31 19:44:19 +0000

    Uploading api and web app classwork

--- API/script.js
+++ API/script.js
@@ -1,33 +1,83 @@
-let posts = []
+// Web Services - One application makes a call to another application through internet
+// types: Xml based: SOAP web service
+//                 : RESTful Web service : json/xml/text/html
 
-function getData(){
-    const url = "https://jsonplaceholder.typicode.com/users"
+//let users = []
+
+function getData() {
+    const url = 'http://localhost:5002/data'
     fetch(url)
-        .then(res => res.json())
-        .then(data => {
-        posts = data
-        })
+    .then(response => response.json())
+     .then(data => {
+         if(data.length > 0){
+            const newTable = document.createElement("table")
+            newTable.innerHTML = "<thead><th>User Name</th><th>Age</th></thead>"
+             data.forEach((user) =>{
+                const newRow = document.createElement("tr")
+                const tdName = document.createElement("td")
+                const tdAge = document.createElement("td")
+                tdName.textContent = user.stud_name
+                tdAge.textContent = user.stud_age
+                newRow.appendChild(tdName)
+                newRow.appendChild(tdAge)
+                newTable.appendChild(newRow)
+            })
+            let userContainer = document.getElementById('posts')
+             userContainer.appendChild(newTable)
+         }
+     })
     .catch(error => {
         console.log(error)
     })
+    //renderPost()
+   // renderTable()
+}    
 
-    renderPost()
-}
+// function renderTable(){
+//     const newTable = document.createElement("table")
+//     newTable.innerHTML = "<thead><th>User Name</th><th>Email</th></thead>"
+//     users.forEach((user) =>{
+//         const newRow = document.createElement("tr")
+//         const tdName = document.createElement("td")
+//         const tdEmail = document.createElement("td")
+//         tdName.textContent = user.name
+//         tdEmail.textContent = user.email
+//         newRow.appendChild(tdName)
+//         newRow.appendChild(tdEmail)
+//         newTable.appendChild(newRow)
+//     })
+//     let userContainer = document.getElementById('users')
+//     userContainer.appendChild(newTable)
+// }
 
-function renderPost(){
-   
-    let postContainer = document.getElementById('posts')
-    postContainer.innerHTML = ''
-    posts.forEach((post) =>{
-        let postElement = document.createElement('div')
-        postElement.className = 'card mb-3'
-    
-        postElement.innerHTML = `
-            <div class='card-body'>
-                <h3 clss="card-title">${post.name}</h3>
-                <p class="card-text">${post.email}</p>       
-            </div>    
-        `
-        postContainer.appendChild(postElement)
-    })
-}
+// function getData() {
+//     const url = "https://jsonplaceholder.typicode.com/users"
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             posts = data
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+
+//     renderPost()
+// }
+
+// function renderPost() {
+
+//     let postContainer = document.getElementById('posts')
+//     postContainer.innerHTML = ''
+//     posts.forEach((post) => {
+//         let postElement = document.createElement('div')
+//         postElement.className = 'card mb-3'
+
+//         postElement.innerHTML = `
+//             <div class='card-body'>
+//                 <h3 clss="card-title">${post.name}</h3>
+//                 <p class="card-text">${post.email}</p>       
+//             </div>    
+//         `
+//         postContainer.appendChild(postElement)
+//     })
+// }
